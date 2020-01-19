00af20b8847bc80d9863463882db802db3b5540f
Ilona-CT <Ilona-CT@users.noreply.github.com>
2020-01-19 18:30:12 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- hello.c
+++ hello.c
@@ -3,6 +3,8 @@
 
 int main(void)
 {
+    //ask for name
     string name = get_string("What is your name?\n");
+    //print out hello + string name input collected line before
     printf("hello, %s\n", name);
 }
