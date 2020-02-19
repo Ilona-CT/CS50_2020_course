b73c9531782db64b2e4c2e99adf002218990cbea
Ilona-CT <Ilona-CT@users.noreply.github.com>
2020-02-19 19:13:48 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -161,7 +161,7 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    int n = candidate_count / 2;
+    int n = voter_count / 2;
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].eliminated == true)
