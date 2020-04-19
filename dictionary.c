d9be02328848ba0bfdffd8c4a96eb39ba06b41cb
Ilona-CT <Ilona-CT@users.noreply.github.com>
2020-04-19 19:22:15 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -31,14 +31,14 @@ unsigned int word_count;
 bool check(const char *word)
 {
     // Convert whole word to lowercase
-    char *low_word = malloc(sizeof(word));
-    for(int i = 0, j = strlen(word); i < j + 1; i++)
+    int j = strlen(word);
+    char low_word[j];
+    for(int i = 0; i < j + 1; i++)
     {
         low_word[i] = tolower(word[i]);
     }
 
     int h = hash(low_word);
-    free(low_word);
     for (node *tmp = table[h]; tmp != NULL; tmp = tmp->next)
     {
         if (strcasecmp(word, tmp->word) == 0)
@@ -55,7 +55,7 @@ unsigned int hash(const char *word)
     // Hashtable copied from https://www.reddit.com/r/cs50/comments/1x6vc8/pset6_trie_vs_hashtable/
     // Also inspired by http://www.cs.yale.edu/homes/aspnes/pinewiki/C(2f)HashTables.html
     unsigned int hash = 0;
-    for (int i = 0, n = strlen(word); i < n + 1; i++)
+    for (int i = 0, n = strlen(word); i < n; i++)
         hash = (hash << 2) ^ word[i];
     return hash % N;
 }
